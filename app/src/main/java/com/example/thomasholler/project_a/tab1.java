2f1c587de55a655b2161cfe24b2fa70ef5d74fa6
Richard Nguyen <richard.ng55@hotmail.com>
2018-11-14 20:55:06 -0800

    adding combined project into git

---
+++ app/src/main/java/com/example/thomasholler/project_a/tab1.java
@@ -0,0 +1,24 @@
+package com.example.thomasholler.project_a;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class tab1 extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_one, container, false);
+        return rootView;
+
+
+    }
+}
